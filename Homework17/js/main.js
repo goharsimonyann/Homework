24c99d64fb5462dcfb55be4907623e0d8af2d216
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-06-27 14:02:43 +0400

    update

--- Homework17/js/main.js
+++ Homework17/js/main.js
@@ -1,80 +1,87 @@
 /////////////     SPECIAL ARRAY PROBLEM   /////////////////
 
-// const testOne = [2, 7, 4, 9, 6, 1, 6, 3]; //true
-// const testTwo = [2, 7, 9, 2, 6, 1, 6, 3]; //false
-// const testThree = [2, 7, 9, 1, 6, 1, 6, 3]; //false
-// const testFour = [2, 3, 2, 3]; //true
+const testOne = [2, 7, 4, 9, 6, 1, 6, 3]; //true
+const testTwo = [2, 7, 9, 2, 6, 1, 6, 3]; //false
+const testThree = [2, 7, 9, 1, 6, 1, 6, 3]; //false
+const testFour = [2, 3, 2, 3]; //true
 
-// function isSpecial(arr = []) {
-//   let index = 0;
-//   while (index < arr.length) {
-//     if (index % 2 === 0 && arr[index] % 2 === 0) {
-//       index++;
-//       continue;
-//     }
-//     if (index % 2 !== 0 && arr[index] % 2 !== 0) {
-//       index++;
-//       continue;
-//     } else {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+function isSpecial(arr = []) {
+  let index = 0;
+  while (index < arr.length) {
+    if (index % 2 === 0 && arr[index] % 2 === 0) {
+      index++;
+      continue;
+    }
+    if (index % 2 !== 0 && arr[index] % 2 !== 0) {
+      index++;
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
 
-// console.log(isSpecial(testOne)); // output:  TRUE
-// console.log(isSpecial(testTwo)); // output:  FALSE
-// console.log(isSpecial(testThree)); // output:  FALSE
-// console.log(isSpecial(testFour)); // output:  TRUE
-// console.log("////////////////////////");
+//    TESTS:
+
+console.log("Array is special: " + isSpecial(testOne)); // output:  TRUE
+console.log("Array is special: " + isSpecial(testTwo)); // output:  FALSE
+console.log("Array is special: " + isSpecial(testThree)); // output:  FALSE
+console.log("Array is special: " + isSpecial(testFour)); // output:  TRUE
+console.log("\n");
 
 /////////////    ALL TRUTHY VALUE PROBLEM   /////////////////
 
-// let arrayOne = [true, true, true]; //true
-// let arrayTwo = [true, false, true]; //false
-// let arrayThree = 0; //false
-// let arrayFour = [5, 4, 3, 2]; //true
-// let arrayFive = [5];
+let arrayOne = [true, true, true]; //true
+let arrayTwo = [true, false, true]; //false
+let arrayThree = ["", NaN, undefined]; //false
+let arrayFour = [5, 4, 3, 2]; //true
+let arrayFive = [5, true, "hello", 0]; //false
 
-// function allTruthy(arr = []) {
-//   let index = 0;
-//   while (index < arr.length) {
-//     if (arr[index] === 0 || false || null || undefined || " " || NaN) {
-//       return false;
-//     } else {
-//       index++;
-//       continue;
-//     }
-//   }
-//   return true;
-// }
+function allTruthy(arr = []) {
+  let index = 0;
+  while (index < arr.length) {
+    if (Boolean(arr[index]) === false) {
+      return false;
+    } else {
+      index++;
+      continue;
+    }
+  }
+  return true;
+}
 
-// console.log(allTruthy(arrayOne));
-// console.log(allTruthy(arrayTwo));
-// console.log(allTruthy(arrayThree));
-// console.log(allTruthy(arrayFour));
+//    TESTS:
+
+console.log("The statement is " + allTruthy(arrayOne)); //  output: TRUE
+console.log("The statement is " + allTruthy(arrayTwo)); //  output: FALSE
+console.log("The statement is " + allTruthy(arrayThree)); //  output: FALSE
+console.log("The statement is " + allTruthy(arrayFour)); //  output: TRUE
+console.log("The statement is " + allTruthy(arrayFive));
+0; //  output: FALSE
+console.log("\n");
 
 /////////////    Mean of All Digits   /////////////////
 
-// function mean(number) {
-//   let sumDigits = 0;
-//   let result = 0;
-//   let numDigits = 0;
-//   let forOutput = number;
-//   while (+number != 0) {
-//     number = Number(number);
-//     sumDigits += number % 10;
-//     number = number / 10;
-//     number = Math.trunc(number);
-//     numDigits++;
-//   }
-//   result = sumDigits / numDigits;
-//   console.log(`The mean of ${forOutput} is ${result}`);
-// }
+function mean(number) {
+  let sumDigits = 0;
+  let result = 0;
+  let numDigits = 0;
+  let forOutput = number;
+  while (+number != 0) {
+    number = Number(number);
+    sumDigits += number % 10;
+    number = number / 10;
+    number = Math.trunc(number);
+    numDigits++;
+  }
+  result = sumDigits / numDigits;
+  console.log(`The mean of ${forOutput} is ${result}`);
+}
 
-// //    TESTS:
+//    TESTS:
 
-// mean(42); // output : 3
-// mean(12345); // output : 3
-// mean(666); // output : 6
-// mean(1456); // output : 4
+mean(42); // output : 3
+mean(12345); // output : 3
+mean(666); // output : 6
+mean(1456); // output : 4
