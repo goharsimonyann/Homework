b3d38c403bc291712cd86ecd2a2f2ff29d341fb1
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-06-29 21:45:55 +0400

    update

--- Homework18/js/secondLargest.js
+++ Homework18/js/secondLargest.js
@@ -1,6 +1,6 @@
-    //  Find the Second Largest Number
+//  Find the Second Largest Number
 
-    //  Create a function that takes an array of numbers and returns the second largest number.
+//  Create a function that takes an array of numbers and returns the second largest number.
 
 function secondLargest(arr) {
   let result = 0;
@@ -11,14 +11,14 @@ function secondLargest(arr) {
   return result;
 }
 
-    //  TESTS
+//  TESTS
 
 let testOne = [10, 40, 30, 20, 50];
 let testTwo = [25, 143, 89, 13, 105];
 let testThree = [54, 23, 11, 17, 10];
 let testFour = [54, 1458, 11, 117, 10];
 
-console.log(`The second largest number is ${secondLargest(testOne)}`);      //  OUTPUT: 40
-console.log(`The second largest number is ${secondLargest(testTwo)}`);      //  OUTPUT: 105  
-console.log(`The second largest number is ${secondLargest(testThree)}`);    //  OUTPUT: 23
-console.log(`The second largest number is ${secondLargest(testFour)}`);     //  OUTPUT: 117
+console.log(`The second largest number is ${secondLargest(testOne)}`); //  OUTPUT: 40
+console.log(`The second largest number is ${secondLargest(testTwo)}`); //  OUTPUT: 105
+console.log(`The second largest number is ${secondLargest(testThree)}`); //  OUTPUT: 23
+console.log(`The second largest number is ${secondLargest(testFour)}`); //  OUTPUT: 117
