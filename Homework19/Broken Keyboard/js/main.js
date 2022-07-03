21a1699b533e3a7b416fc45f4f0895944c10f677
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-04 02:07:23 +0400

    update

--- Homework19/Broken Keyboard/js/main.js
+++ Homework19/Broken Keyboard/js/main.js
@@ -1,25 +1,25 @@
-// //      ## **Broken Keyboard.**
+//      ## **Broken Keyboard.**
 
-// // Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s). 
+// Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s).
 
-// function brokenKeyboard(str1, str2) {
-//   let i = 0;
-//   let arr = [];
-//   while (i < str1.length) {
-//     if (str1[i] === str2[i]) {
-//       ++i;
-//       continue;
-//     } else {
-//       arr.push(str1[i]);
-//       ++i;
-//     }
-//   }
-//   arr = arr.filter((c, index) => {
-//     return arr.indexOf(c) === index;
-//   });
-//   return arr;
-// }
+function brokenKeyboard(str1, str2) {
+  let i = 0;
+  let arr = [];
+  while (i < str1.length) {
+    if (str1[i] === str2[i]) {
+      ++i;
+      continue;
+    } else {
+      arr.push(str1[i]);
+      ++i;
+    }
+  }
+  arr = arr.filter((c, index) => {
+    return arr.indexOf(c) === index;
+  });
+  return arr;
+}
 
-// console.log(brokenKeyboard("happy birthday", "hawwy birthday"));
-// console.log(brokenKeyboard("starry night", "starrq light"));
-// console.log(brokenKeyboard("beethoven", "affthoif5"));
+console.log(brokenKeyboard("happy birthday", "hawwy birthday"));
+console.log(brokenKeyboard("starry night", "starrq light"));
+console.log(brokenKeyboard("beethoven", "affthoif5"));
--- Homework19/Items Purchased/js/main.js
+++ Homework19/Items Purchased/js/main.js
@@ -1,31 +1,31 @@
-// //  GOT ENOUGH MONEY?
-// // Create a function that returns any of the items you can afford in
-// // the `store` with the money you have in your `wallet`. Sort the list in alphabetical order.
+//  GOT ENOUGH MONEY?
+// Create a function that returns any of the items you can afford in
+// the `store` with the money you have in your `wallet`. Sort the list in alphabetical order.
 
-// function itemsPurchased(objects, wallet) {
-//   let entries = Object.entries(objects),
-//     vals = Object.values(objects),
-//     price = Number(wallet.slice(1));
-//   let values = [],
-//     canAfford = [];
-//   vals.forEach((value) => {
-//     value = Number(value.slice(1));
-//     values.push(value);
-//   });
+function itemsPurchased(objects, wallet) {
+  let entries = Object.entries(objects),
+    vals = Object.values(objects),
+    price = Number(wallet.slice(1));
+  let values = [],
+    canAfford = [];
+  vals.forEach((value) => {
+    value = Number(value.slice(1));
+    values.push(value);
+  });
 
-//   values.filter((item, index) => {
-//     if (item <= price) {
-//       canAfford.push(String(entries[index][0]));
-//     }
-//   });
-//   let result = canAfford.sort();
-//   canAfford.length === 0 ? console.log("Nothing") : console.log(result);
-// }
+  values.filter((item, index) => {
+    if (item <= price) {
+      canAfford.push(String(entries[index][0]));
+    }
+  });
+  let result = canAfford.sort();
+  canAfford.length === 0 ? console.log("Nothing") : console.log(result);
+}
 
-// const testOne = { Water: "$1", Bread: "$3", TV: "$1,000", Fertilizer: "$20" };
-// const testTwo = { Apple: "$4", Honey: "$3", Fan: "$14", Bananas: "$4", Pan: "$100", Spoon: "$2",};
-// const testThree = { Phone: "$999", Speakers: "$300", Laptop: "$5,000", PC: "$1200",};
+const testOne = { Water: "$1", Bread: "$3", TV: "$1,000", Fertilizer: "$20" };
+const testTwo = { Apple: "$4", Honey: "$3", Fan: "$14", Bananas: "$4", Pan: "$100", Spoon: "$2",};
+const testThree = { Phone: "$999", Speakers: "$300", Laptop: "$5,000", PC: "$1200",};
 
-// itemsPurchased(testOne, "$300");
-// itemsPurchased(testTwo, "$100");
-// itemsPurchased(testThree, "$1");
+itemsPurchased(testOne, "$300");
+itemsPurchased(testTwo, "$100");
+itemsPurchased(testThree, "$1");
