d46405deb2ef9e1d23586880824b1ab1187af06f
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-05 00:52:16 +0400

    update

--- Homework19/Box/js/main.js
+++ Homework19/Box/js/main.js
@@ -1,14 +1,14 @@
-// function makeBox(n) {
-//   for (let i = 1; i <= n; ++i) {
-//     for (let j = 1; j <= n; ++j) {
-//       if ((i == 1 && j == 1) || (i == n && j == n)) {
-//         console.log("*");
-//       } else {
-//         console.log(" ");
-//       }
-//       console.log("\n");
-//     }
-//   }
-// }
+function makeBox(n) {
+  for (let i = 1; i <= n; ++i) {
+    for (let j = 1; j <= n; ++j) {
+      if ((i == 1 && j == 1) || (i == n && j == n)) {
+        console.log("*");
+      } else {
+        console.log(" ");
+      }
+    }
+    console.log("\n");
+  }
+}
 
-// makeBox(3);
+makeBox(3);
