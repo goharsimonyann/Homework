e481cdc52961f147380d5902f9efbcee259594e5
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-05 00:51:35 +0400

    update

--- Homework19/Notes/js/main.js
+++ Homework19/Notes/js/main.js
@@ -1,20 +1,17 @@
-// function getNotes(array) {
-//   let i = 0;
-//   let entries = [];
-//   while (i < array.length) {
-//     entries.push(Object.entries(array[i]));
-//     ++i;
-//   }
-//   entries.forEach((elem) => {
-//     elem[1][1].sort((a, b) => b - a);
-//   });
-//   let topNotes = entries[]
-//   console.log(entries);
-// }
+// //  Get Students with Names and Top Notes.
 
 // const students = [
 //   { name: "John", notes: [3, 5, 4] },
 //   { name: "Max", notes: [1, 4, 6] },
 //   { name: "Zygmund", notes: [1, 2, 3] },
 // ];
-// getNotes(students);
+
+// const getTopNotes = students.map((elem) => {
+//   elem.notes.sort((a, b) => b - a);
+//   return {
+//     name: elem.name,
+//     topNotes: elem.notes[0],
+//   };
+// });
+
+// console.log(getTopNotes);
