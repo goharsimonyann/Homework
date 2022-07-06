58427ec266e1a4db687b0ddf5f2930e68a5bf43d
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-06 17:43:34 +0400

    update

--- Homework19/Pizza Points/js/main.js
+++ Homework19/Pizza Points/js/main.js
@@ -1,36 +1,32 @@
-// // Google is launching a network of autonomous pizza delivery drones and wants you to 
-// // create a flexible rewards system (Pizza Points™) that can be tweaked in the future. 
-// // The rules are simple: if a customer has made at least *N* orders of at least *Y* price, 
-// // they get a FREE pizza!
+// // PIZZA POINTS
 
-// // Create a function that takes an object of customers, a minimum number of orders and a 
-// // minimum order price. Return an array of customers that are eligible for a free pizza.
+// function pizzaPoints(customer, order, price){
 
-// function pizzaPoints(obj, order, price){  //obj order = 5  price = 20
-//     let entries = Object.entries(obj);
-//     console.log(entries);
-//     let i = 0;
-//     let priceArr = [];
-//     while(i< entries.length){
-//       priceArr.push(entries[i][1].filter(elem => elem >= price));
-//       ++i;
-//     }
-//     // CONSOLENERE CHI ANUM
-    
-//     while(i<priceArr.length){
-//       if(priceArr[i].lenght >= order) {
-//         console.log("success");
+//     let freePizza =  Object.keys(customer).reduce((item1, item2) => {
+//       let customerOne = customersObj[item1].filter(elem => elem >= price) ;
+//       let customerTwo = customersObj[item2].filter(elem => elem >= price) ;
+      
+//       if(customerOne.length >= order && customerTwo.length >= order){
+//         return item1 + " " + item2;
+//       } else if(customerOne.length >= order){
+//         return item1;
+//       } else if(customerTwo.length >= order){
+//         return item2;
 //       } else {
-//         console.log("fail");
+//         return [];
 //       }
-//     }
-//     console.log(priceArr);
+//     });
+    
+//     return freePizza;
 //   }
   
 //   let customersObj = {
-//       "Batman": [22, 30, 11, 52, 27, 12],
-//       "Spider-Man": [5, 17, 3, 33, 40, 25]
+//       "Batman": [22, 30, 11, 17, 15, 52, 27, 12],
+//       "Spider-Man": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45]
 //   }
   
-//   pizzaPoints(customersObj, 5, 20);
-
+//   console.log(pizzaPoints(customersObj, 5, 20));
+//   console.log(pizzaPoints(customersObj, 3, 10));
+//   console.log(pizzaPoints(customersObj, 5, 100));
+  
+  
