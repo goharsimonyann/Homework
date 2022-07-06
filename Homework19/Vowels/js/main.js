02ef3e7a4b4a8afb19f97fa7302dffd60e72f89c
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-07 00:02:13 +0400

    update

--- Homework19/Vowels/js/main.js
+++ Homework19/Vowels/js/main.js
@@ -5,22 +5,41 @@
 
 // Given a censored string and a string of the censored vowels, return the original uncensored string.
 
+
 function uncensor(str1, str2) {
-  let newStr = "";
-  let count = 0,
-    i = 0;
-  while (i < str1.length) {
-    if (str1[i] === "*") {
-      newStr += str2[count];
-      ++count;
-      ++i;
-    } else {
-      newStr += str1[i];
-      ++i;
+  let uncensoredStr = "";
+  let index = 0;
+  for(let i = 0; i < str1.length; i++){
+    if(str1[i] === "*"){
+      uncensoredStr += str2[index];
+      ++index;
+    } else{
+      uncensoredStr += str1[i];
     }
   }
-  return newStr;
+    return uncensoredStr;
 }
 
 console.log(uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo"));
 console.log(uncensor("abcd", ""));
+console.log(uncensor("*PP*RC*S*", "UEAE"));
+
+
+//  USING WHILE LOOP
+
+// function uncensor(str1, str2) {
+//   let newStr = "";
+//   let count = 0,
+//     i = 0;
+//   while (i < str1.length) {
+//     if (str1[i] === "*") {
+//       newStr += str2[count];
+//       ++count;
+//       ++i;
+//     } else {
+//       newStr += str1[i];
+//       ++i;
+//     }
+//   }
+//   return newStr;
+// }
