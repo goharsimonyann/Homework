3aa0f195bcf6e1a8e37a34c80d654162dc8608d5
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-06 19:44:51 +0400

    update

--- Homework19/Expensive Jewerly/js/mostExpensive.js
+++ Homework19/Expensive Jewerly/js/mostExpensive.js
@@ -2,19 +2,10 @@
 
 // Create a function that gets the name of the piece of jewelry with the highest price and
 // returns "The most expensive one is the {name of jewelry piece}"
-function mostExpensive(obj) {
-  let objEntries = Object.entries(obj);
-  let objValues = Object.values(obj);
-  let sortedValues = objValues.sort((a, b) => b - a);
 
-  let maxPrice = sortedValues[0];
-  let i = 0;
-  while (i < objEntries.length) {
-    if (objEntries[i][1] === maxPrice) {
-      console.log(`The most expensive one is the ${objEntries[i][0]}`);
-    }
-    ++i;
-  }
+function mostExpensive(obj) {
+  let expensive = Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b));
+  return `The most expensive one is the ${expensive}`;
 }
 
 const testOne = {
@@ -28,5 +19,22 @@ const testTwo = {
   "Diamond Ring": 3500,
 };
 
-mostExpensive(testOne);
-mostExpensive(testTwo);
+console.log(mostExpensive(testOne));
+console.log(mostExpensive(testTwo));
+
+// LONG SOLUTION
+
+// function mostExpensive(obj) {
+//   let objEntries = Object.entries(obj);
+//   let objValues = Object.values(obj);
+//   let sortedValues = objValues.sort((a, b) => b - a);
+
+//   let maxPrice = sortedValues[0];
+//   let i = 0;
+//   while (i < objEntries.length) {
+//     if (objEntries[i][1] === maxPrice) {
+//       console.log(`The most expensive one is the ${objEntries[i][0]}`);
+//     }
+//     ++i;
+//   }
+// }
