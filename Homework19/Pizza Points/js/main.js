188cc510025be1c8ecc09c8e85053a261cad4d76
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-06 17:44:12 +0400

    update

--- Homework19/Pizza Points/js/main.js
+++ Homework19/Pizza Points/js/main.js
@@ -1,32 +1,29 @@
-// // PIZZA POINTS
+// PIZZA POINTS
 
-// function pizzaPoints(customer, order, price){
+function pizzaPoints(customer, order, price) {
+  let freePizza = Object.keys(customer).reduce((item1, item2) => {
+    let customerOne = customersObj[item1].filter((elem) => elem >= price);
+    let customerTwo = customersObj[item2].filter((elem) => elem >= price);
 
-//     let freePizza =  Object.keys(customer).reduce((item1, item2) => {
-//       let customerOne = customersObj[item1].filter(elem => elem >= price) ;
-//       let customerTwo = customersObj[item2].filter(elem => elem >= price) ;
-      
-//       if(customerOne.length >= order && customerTwo.length >= order){
-//         return item1 + " " + item2;
-//       } else if(customerOne.length >= order){
-//         return item1;
-//       } else if(customerTwo.length >= order){
-//         return item2;
-//       } else {
-//         return [];
-//       }
-//     });
-    
-//     return freePizza;
-//   }
-  
-//   let customersObj = {
-//       "Batman": [22, 30, 11, 17, 15, 52, 27, 12],
-//       "Spider-Man": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45]
-//   }
-  
-//   console.log(pizzaPoints(customersObj, 5, 20));
-//   console.log(pizzaPoints(customersObj, 3, 10));
-//   console.log(pizzaPoints(customersObj, 5, 100));
-  
-  
+    if (customerOne.length >= order && customerTwo.length >= order) {
+      return item1 + " " + item2;
+    } else if (customerOne.length >= order) {
+      return item1;
+    } else if (customerTwo.length >= order) {
+      return item2;
+    } else {
+      return [];
+    }
+  });
+
+  return freePizza;
+}
+
+let customersObj = {
+  Batman: [22, 30, 11, 17, 15, 52, 27, 12],
+  "Spider-Man": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45],
+};
+
+console.log(pizzaPoints(customersObj, 5, 20));
+console.log(pizzaPoints(customersObj, 3, 10));
+console.log(pizzaPoints(customersObj, 5, 100));
