931b4bfce7a7dd2c3ae8740ca0c0548e445757af
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-06 23:50:43 +0400

    update

--- Homework19/Broken Keyboard/js/main.js
+++ Homework19/Broken Keyboard/js/main.js
@@ -3,23 +3,42 @@
 // Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s).
 
 function brokenKeyboard(str1, str2) {
-  let i = 0;
-  let arr = [];
-  while (i < str1.length) {
-    if (str1[i] === str2[i]) {
-      ++i;
-      continue;
-    } else {
-      arr.push(str1[i]);
-      ++i;
-    }
-  }
-  arr = arr.filter((c, index) => {
-    return arr.indexOf(c) === index;
+  let brokenKeys = [];
+ for(let i =0;i<str1.length;i++){
+   if(str1[i] === str2[i]){
+     continue;
+   } else{
+     brokenKeys.push(str1[i]);
+   }
+ }
+ 
+ let foundBrokenKeys = brokenKeys.filter((elem, index) => {
+    return brokenKeys.indexOf(elem) === index;
   });
-  return arr;
+  
+  console.log(foundBrokenKeys);
 }
 
-console.log(brokenKeyboard("happy birthday", "hawwy birthday"));
-console.log(brokenKeyboard("starry night", "starrq light"));
-console.log(brokenKeyboard("beethoven", "affthoif5"));
+brokenKeyboard("happy birthday", "hawwy birthday");
+brokenKeyboard("starry night", "starrq light");
+brokenKeyboard("beethoven", "affthoif5");
+
+//  ANOTHER SOLUTION USING WHILE 
+
+// function brokenKeyboard(str1, str2) {
+//   let i = 0;
+//   let arr = [];
+//   while (i < str1.length) {
+//     if (str1[i] === str2[i]) {
+//       ++i;
+//       continue;
+//     } else {
+//       arr.push(str1[i]);
+//       ++i;
+//     }
+//   }
+//   arr = arr.filter((c, index) => {
+//     return arr.indexOf(c) === index;
+//   });
+//   return arr;
+// }
