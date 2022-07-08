a7da1c709135391c932550cbc5219ac38c7efc46
goharsimonyann <82412056+goharsimonyann@users.noreply.github.com>
2022-07-09 00:30:24 +0400

    update

--- Homework19/Box/js/main.js
+++ Homework19/Box/js/main.js
@@ -1,16 +1,21 @@
 //  DOES NOT WORK
 
 function makeBox(n) {
-  for (let i = 1; i <= n; ++i) {
-    for (let j = 1; j <= n; ++j) {
-      if ((i == 1 && j == 1) || (i == n && j == n)) {
-        console.log("*");
+  let line = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (i === 1 || i === n) {
+        line += "#";
       } else {
-        console.log(" ");
+        line += j === 1 || j === n ? "#" : " ";
       }
     }
-    console.log("\n");
+    console.log(line);
+    line = "";
   }
+  return line;
 }
-
+console.log(makeBox(5));
 console.log(makeBox(3));
+console.log(makeBox(2));
+console.log(makeBox(1));
--- Homework19/Pizza Points/js/main.js
+++ Homework19/Pizza Points/js/main.js
@@ -20,10 +20,36 @@ function pizzaPoints(customer, order, price) {
 }
 
 let customersObj = {
-  "Batman": [22, 30, 11, 17, 15, 52, 27, 12],
+  Batman: [22, 30, 11, 17, 15, 52, 27, 12],
   "Spider-Man": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45],
 };
 
-console.log(pizzaPoints(customersObj, 5, 20));
-console.log(pizzaPoints(customersObj, 3, 10));
-console.log(pizzaPoints(customersObj, 5, 100));
+function foo(obj, order, price) {
+  return Object.keys(obj).filter(
+    (i) => obj[i].filter((cost) => cost >= price).length > order
+  );
+}
+
+console.log(foo(customersObj, 5, 20));
+// function pizzaPoints(obj, order, price) {
+//   let freePizza = Object.entries(obj).map((item) => {
+//     let filteredOrder = obj[item].filter((elem) => elem >= price);
+//     for (let i = 0; i < filteredOrder.length; i++) {
+//       if (filteredOrder[i].length >= order) {
+//         return item;
+//       }
+//     }
+//   });
+//   return freePizza;
+// }
+
+// console.log(pizzaPoints(customersObj, 5, 20));
+
+// // let customersObj = {
+// //   "Batman": [22, 30, 11, 17, 15, 52, 27, 12],
+// //   "Spider-Man": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45],
+// // };
+
+// console.log(pizzaPoints(customersObj, 5, 20));
+// console.log(pizzaPoints(customersObj, 3, 10));
+// console.log(pizzaPoints(customersObj, 5, 100));
